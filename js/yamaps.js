a81940218580810815285c72eefaf5f84cdae345
Pavel Klimov <pavel.klimov@osinit.com>
2018-12-04 16:56:56 +0300

    add contact.html

--- js/yamaps.js
+++ js/yamaps.js
@@ -6,20 +6,8 @@ function init(){
     zoom: 7
   },{
     serchControlProvider: 'yandex#search'
-  }),
+  });
 
-  MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'),
 
-  myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-    hintContent: 'Собственный значок метки',
-    balloonContent: 'Это красивая метка'
-  }, {
-    iconLayout: 'default#image',
-    iconImageHref: 'images/myIcon.gif',
-    iconImageSize: [30, 42],
-    iconImageOffset: [-5, -38]
-  }),
-  myMap.geoObjects
-    .add(myPlacemark)
 
 }
